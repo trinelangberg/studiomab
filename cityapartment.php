3317866e3f7b079fe504293ef71ec8f0db55f35e
anastasiyeah <anastasiyeah@Anastasiyas-MacBook-Pro.local>
2018-04-30 15:21:31 +0200

    hmm

--- cityapartment.php
+++ cityapartment.php
@@ -59,7 +59,7 @@
 	  
 	  <!--PROJECT IMAGES WITH TEXT START -->
 	  
-	  <div class="container">
+	  <article class="container">
 	  	<div class="img-projects">
 		  	 <h1 id="projects-h1">WE AIM TO CREATE A LASTING SOCIAL, <br> ECONOMIC AND ENVIROMENTALT EFFECT <br> WITH EVERYTHING THAT WE TAKE UPON US</h1>
 		  	 <h2 id="bakery">city apartment <br> redesign of an old bakery </h2>
@@ -68,7 +68,7 @@
 		      <h2 id="bakery-front">frontal view of city apartment <br> redesign of an old bakery </h2>
 			  <img src="images/bakery-frontview.jpg" alt="Redesigned bakery, front view" width="100%">
 	  	</div>
-	 </div>
+	 </article>
 	  
 	    <!--PROJECT IMAGES WITH TEXT END -->
 	
